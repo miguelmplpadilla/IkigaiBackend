165128845e42b549141fb15df7faf294b4da49ce
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2025-08-25 16:26:52 +0200

    Send confirmation email test

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import Stripe from "stripe";
 import dotenv from "dotenv";
+import axios from "axios";
 
-// Cargar variables de entorno
+
 dotenv.config();
 
 const app = express();
@@ -42,6 +43,10 @@ app.post("/api/create-checkout-session", async (req, res) => {
             cancel_url: cancel_url,
         });
 
+        if (session.url === success_url) {
+            await SendConfirmationEmail();
+        }
+
         res.json({ id: session.id });
     } catch (err: any) {
         console.error("❌ Error en /create-checkout-session:", err.message);
@@ -54,3 +59,29 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor backend escuchando en puerto ${PORT}`);
 });
+
+const serviceID = 'service_j9zpngi';
+const templateID = 'template_fmhzfx4';
+const userID = 'pSQpDE6EkrRZeTkvj'; // (user_id de EmailJS)
+
+async function SendConfirmationEmail() {
+    const templateParams = {
+        from_name: 'Miguel',
+        to_name: 'Cliente',
+        message: 'Gracias por tu compra. Aquí tienes los detalles del pedido.',
+        reply_to: 'miguelpadillal@hotmail.es'
+    };
+
+    try {
+        const response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', {
+            service_id: serviceID,
+            template_id: templateID,
+            user_id: userID,
+            template_params: templateParams
+        });
+
+        console.log('Correo enviado con éxito:', response.data);
+    } catch (error: any) {
+        console.error('Error al enviar el correo:', error.response?.data || error.message);
+    }
+}
